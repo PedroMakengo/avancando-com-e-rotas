8b42ef2f2e4854ec7e03de8cbf862c141efd5971
PedroMakengo <pedromakengomiguel@gmail.com>
2022-11-07 21:00:46 +0000

    Consumindo a minha api

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,35 @@
 <template>
   <div class="app">
-    <h1>Aprendendo VueJs</h1>
+    <header>
+      <strong>Vue Nutri</strong>
+    </header>
+
+    <article v-for="(item, index) in nutri" :key="index">
+      <strong>{{ item.titulo }}</strong>
+      <img :src="item.capa" />
+      <span>Categoria: {{ item.categoria }}</span>
+      <p>{{ item.subtitulo }}</p>
+    </article>
   </div>
 </template>
 
 <script>
+import api from "./services/api";
+
 export default {
   name: "App",
-  data() {},
+  data() {
+    return {
+      nutri: [],
+    };
+  },
+  async created() {
+    const response = await api.get("?api=posts");
+    const data = await response.data;
+
+    this.nutri = data;
+    console.log(data);
+  },
 };
 </script>
 
